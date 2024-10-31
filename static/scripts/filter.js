5a7b64fc303386fd52bfae4d01c4a85e5aa5f102
arashi <kairbektamerlan@gmail.com>
2024-10-31 15:58:04 +0500

    .

--- static/scripts/filter.js
+++ static/scripts/filter.js
@@ -31,5 +31,81 @@ const items = [
     }
 ]
 
-const finalItems = [];
+const cardContainer = document.querySelector('.card-container');
 
+const finalItems = new Set();
+
+const toggledFilters = [];
+
+const filterBtns = document.querySelectorAll(".filter-btn");
+filterBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle("active-choice");
+        if(toggledFilters.includes(btn.textContent)) {
+            delete toggledFilters[toggledFilters.indexOf(btn.textContent)];
+            if(btn.classList.contains('year')) {
+                items.filter(i => {
+                    if(i.year === btn.textContent) {
+                        finalItems.delete(i);
+                    }
+                })
+            }
+            if(btn.classList.contains('type')) {
+                items.filter(i => {
+                    if(i.type === btn.textContent) {
+                        finalItems.delete(i);
+                    }
+                })
+            }
+        } else {
+            toggledFilters.push(btn.textContent);
+            if(btn.classList.contains('year')) {
+                items.filter(i => {
+                    if(i.year === btn.textContent) {
+                        finalItems.add(i);
+                    }
+                })
+            }
+            if(btn.classList.contains('type')) {
+                items.filter(i => {
+                    if(i.type === btn.textContent) {
+                        finalItems.add(i);
+                    }
+                })
+            }
+        }
+
+        if(toggledFilters.length === 0) {
+            items.forEach(i => {
+                finalItems.add(i);
+            })
+        }
+
+        clearContent(cardContainer);
+        showItems()
+    });
+})
+
+function clearContent(element) {
+    element.innerHTML = ''
+}
+
+function showItems() {
+    finalItems.forEach(i => {
+        cardContainer.insertAdjacentHTML('afterbegin',
+            `
+            <div class="card">
+                <div class="card-element">
+                    Subject: ${i.name}
+                </div>
+                <div class="card-element">
+                    Year: ${i.year}
+                </div>
+                <div class="card-element">
+                    Type: ${i.type}
+                </div>
+            </div>
+            `
+        )
+    });
+}
